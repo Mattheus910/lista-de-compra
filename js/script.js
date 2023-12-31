f661cf54667ca82e8ec2758950564a7386b67038
Mattheus910 <123586175+Mattheus910@users.noreply.github.com>
2023-12-31 17:01:14 -0300

    edição iplementada

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ const valorInput = document.querySelector("#valor");
 const cadastrarBtn = document.querySelector("#list-form button");
 const listForm = document.querySelector("#list-form");
 const editForm = document.querySelector("#edit-form");
-const editBtn = document.querySelector(".edit-btn");
-const cancelBtn = document.querySelector(".cancel-edit-btn");
+const editBtn = document.querySelector("#edit-btn");
+const cancelBtn = document.querySelector("#cancel-edit-btn");
 const list = document.querySelector("#lista");
 const editItemInput = document.querySelector("#edit-item");
 const editQuantidadeInput = document.querySelector("#edit-quantidade");
 const editValorInput = document.querySelector("#edit-valor");
 
-let oldItemValue;
-let oldQuantidadeValue;
-let oldValorValue;
+let nomeAntigo;
+let quantidadeAntiga;
+let valorAntigo;
 
 // Funções
 
@@ -41,6 +41,7 @@ const adicionarItem = () => {
         itens.appendChild(valorItem);
     
         const valorTotalItem = document.createElement("p");
+        valorTotalItem.classList.add("valor-total")
         valorTotalItem.innerText = `R$${(Number(valorInput.value) * Number(quantidadeInput.value)).toFixed(2).replace('.', ',')}`;
         itens.appendChild(valorTotalItem);
     
@@ -76,6 +77,29 @@ const adicionarItem = () => {
 
 };
 
+const updateTodo = (editInputValue, editQuantValue, editValorValue) => {
+
+    const itens = document.querySelectorAll(".itens");
+
+    itens.forEach((item) => {
+
+        let itemTitle = item.querySelector("h4");
+        let quantidadeTitle = item.querySelector(".quantidade-item");
+        let valorTitle = item.querySelector(".valor-item");
+        let valorTotalTitle = item.querySelector(".valor-total")
+        
+        
+
+        if (itemTitle.innerText === nomeAntigo) {
+            itemTitle.innerText = editInputValue;
+            quantidadeTitle.innerText = editQuantValue;
+            valorTitle.innerText = `R$${parseFloat(editValorValue).toFixed(2).replace(".", ",")}`;
+            valorTotalTitle.innerText = `R$${(Number(editQuantValue) * Number(editValorValue)).toFixed(2).replace(".", ",")}`;
+        } 
+
+    });
+};
+
 
 
 // Eventos
@@ -90,18 +114,21 @@ document.addEventListener("click", (e) => {
     const targetEl = e.target;
     const parentEl = targetEl.closest("section");
 
-    oldValue = parentEl.querySelector("h4").innerText;
-    oldQuantidadeValue = parentEl.querySelector(".quantidade-item").innerText;
-    oldValorValue = parentEl.querySelector(".valor-item").innerText;
-    
-    
+    let oldValue;
+    let oldQuantidadeValue;
+    let oldValorValue;
 
+    if (parentEl && parentEl.querySelector("h4")) {
+        oldValue = parentEl.querySelector("h4").innerText;
+        oldQuantidadeValue = parentEl.querySelector(".quantidade-item").innerText;
+        oldValorValue = parentEl.querySelector(".valor-item").innerText;
+    }
 
     if (targetEl.classList.contains("pego")){
         parentEl.classList.toggle("done");
     };
 
-    if (targetEl.classList.contains("excluir") || targetEl.classList.contains("bi bi-x-lg")){
+    if (targetEl.classList.contains("excluir")){
         parentEl.remove();
     };
 
@@ -112,9 +139,13 @@ document.addEventListener("click", (e) => {
 
         editItemInput.value = oldValue;
         editQuantidadeInput.value = oldQuantidadeValue;
-        editValorInput.value = oldValorValue;
-    };
+        editValorInput.value = parseFloat(oldValorValue.replace("R$", "").replace(",", ".")).toFixed(2);
 
+        nomeAntigo = oldValue;
+        quantidadeAntiga = oldQuantidadeValue;
+        valorAntigo = oldValorValue;
+  
+    };
 });
 
 cancelBtn.addEventListener("click", (e) => {
@@ -124,6 +155,23 @@ cancelBtn.addEventListener("click", (e) => {
     editForm.style.display = "none";
     list.style.display = "block";
 });
+
+editBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const editInputValue = editItemInput.value;
+    const editQuantValue = editQuantidadeInput.value;
+    const editValorValue = editValorInput.value;
+
+    if (editInputValue) {
+        updateTodo(editInputValue, editQuantValue, editValorValue);
+    }
+
+    listForm.style.display = "flex";
+    editForm.style.display = "none";
+    list.style.display = "block";
+
+})
 
 
 
